84bf1cd0362fb65badf68b7660ed6fea048df40b
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-07 08:41:18 -0700

    Simply display a book from the library arr in an HTML table

--- js/script.js
+++ js/script.js
@@ -1,9 +1,46 @@
 let myLibrary = []
 
-function Book() {
-    // the constructor
+class Book {
+    constructor(name, author, pages) {
+        this.name   = name
+        this.author = author
+        this.pages  = pages
+    }
+
+    info() {
+        return this.name + ', by ' + this.author + ', ' + this.pages + ' pages'
+    }
 }
 
-function addBookToLibrary() {
+function displayLibrary(arr) {
+    const tb = document.getElementById('tbody')
+    arr.map(b => {
+        const { name, author, pages } = b
+        const tr        = document.createElement('tr')
+        const tdName    = document.createElement('td')
+        const tdAuthor  = document.createElement('td')
+        const tdPages   = document.createElement('td')
+
+        tdName.innerText    = name
+        tdAuthor.innerText  = author
+        tdPages.innerText   = pages
+
+        tr.appendChild(tdName)
+        tr.appendChild(tdAuthor)
+        tr.appendChild(tdPages)
+        tb.appendChild(tr)
+    })
+    return
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+    displayLibrary(myLibrary)
+}
+
+function removeBookFromLibrary(book) {
     // do stuff
 }
+
+let hp = new Book('Harry Potter', 'J.K. Rowling', 245)
+addBookToLibrary(hp)
