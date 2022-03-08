9b75b34f47302e8fda2de097321822c517e05c72
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-08 10:25:21 -0700

    Working on getting a default data view to display, and the function to swap the data display. Need to work on binding the form and the inputs

--- js/other.js
+++ js/other.js
@@ -12,21 +12,29 @@ const dataSections = document.querySelectorAll('.data-section')
 
 let dataDisplay = (function() {
     let dataView = 'table'
+    let tb       = document.getElementById('tbody')
+    let cb       = document.getElementById('cardsDisplayParent')
     
     function _createDataDisplay(value) {
-        if (value === 'table') {
-            console.log('Table')
+        if (dataView === value) {
+            myLibrary.db.forEach(obj => {
+                cb.classList.toggle('hidden')
+                tb.appendChild(obj.tableElement)
+            })
         } else {
-            console.log('Cards')
+            myLibrary.db.forEach(obj => {
+                tb.classList.toggle('hidden')
+                cb.appendChild(obj.cardElement)
+            })
         }
     }
 
     return {
+        defaultView: function() {
+
+        },
         changeView: function(viewValue) {
-            if (viewValue !== dataView) {
-                this.dataView = viewValue
-                _createDataDisplay(viewValue)
-            }
+            _createDataDisplay(viewValue)
         }
     }
 })()
@@ -98,6 +106,99 @@ let myLibrary = (function() {
         return copiedArr
     }
 
+    // --- create HTML elements
+    function _tableElement(book, id) {
+        // -- destructure the book
+        const { name, author, pages, read } = book
+        // -- create HTML elements
+        //      + table row & data
+        const tr            = document.createElement('tr')
+        const tdName        = document.createElement('td')
+        const tdAuthor      = document.createElement('td')
+        const tdPages       = document.createElement('td')
+        const tdRead        = document.createElement('td')
+        const tdDelete      = document.createElement('td')
+        // -- set HTML attributes
+        tdPages.style.textAlign = 'center'
+        tdRead.style.textAlign = 'center'
+        tdDelete.style.textAlign = 'center'
+        // ~~ delete button
+        const deleteBtn     = document.createElement('button')
+        deleteBtn.innerText = 'Delete'
+        deleteBtn.setAttribute('type', 'button')
+        deleteBtn.setAttribute('value', id)
+
+        // --- Delete Btn
+        deleteBtn.addEventListener('click', (e) => {
+            // -- get the parent node (which is the tr)
+            //      + learned from https://stackoverflow.com/questions/13241005/add-delete-row-from-a-table
+            let row = e.target.parentNode.parentNode
+            let btn = e.currentTarget.value
+            // -- pass row to remove function
+            //      + removes this child from the DOM
+            bookObj.removeBookDisplay(btn)
+            // myLibrary.removeBookFromDb(row)
+        })
+
+        tdName.innerText    = name
+        tdAuthor.innerText  = author
+        tdPages.innerText   = pages
+        tdRead.innerText    = read
+        tdDelete.appendChild(deleteBtn)
+
+        tr.appendChild(tdName)
+        tr.appendChild(tdAuthor)
+        tr.appendChild(tdPages)
+        tr.appendChild(tdRead)
+        tr.appendChild(tdDelete)
+
+        return tr
+    }
+    function _cardElement(book, id) {
+        // -- destructure the book
+        const { name, author, pages, read } = book
+        // -- create HTML elements
+        //      + card container and data containers
+        const cardContainer = document.createElement('div')
+        const divName       = document.createElement('div')
+        const divAuthor     = document.createElement('div')
+        const divPages      = document.createElement('div')
+        const divRead       = document.createElement('div')
+        const divDelete     = document.createElement('div')
+        // -- set HTML attributes
+        cardContainer.classList.add('card-container', 'flex')
+        // ~~ delete button
+        const deleteBtn     = document.createElement('button')
+        deleteBtn.innerText = 'Delete'
+        deleteBtn.setAttribute('type', 'button')
+        deleteBtn.setAttribute('value', id)
+
+        // --- Delete Btn
+        deleteBtn.addEventListener('click', (e) => {
+            // -- get the parent node (which is the tr)
+            //      + learned from https://stackoverflow.com/questions/13241005/add-delete-row-from-a-table
+            let btn = e.currentTarget.value
+            // -- pass row to remove function
+            //      + removes this child from the DOM
+            bookObj.removeBookDisplay(btn)
+            // myLibrary.removeBookFromDb(card)
+        })
+
+        divName.innerText    = name
+        divAuthor.innerText  = author
+        divPages.innerText   = pages
+        divRead.innerText    = read
+        divDelete.appendChild(deleteBtn)
+
+        cardContainer.appendChild(divName)
+        cardContainer.appendChild(divAuthor)
+        cardContainer.appendChild(divPages)
+        cardContainer.appendChild(divRead)
+        cardContainer.appendChild(divDelete)
+
+        return cardContainer
+    }
+
     // --- private counter function used to make a
     //     mock ID to easily delete library objects
     function _counter() {
@@ -108,8 +209,10 @@ let myLibrary = (function() {
     //     to the object
     function _createLibraryObject(book, index) {
         let libObj = {
-            data        : book,
-            index       : index
+            data         : book,
+            index        : index,
+            tableElement : _tableElement(book, index),
+            cardElement  : _cardElement(book, index)
         }
         return libObj
     }
