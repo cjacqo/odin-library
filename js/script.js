d8ecb1b3c8ca05cf96bd5e644f6fef749326c062
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-07 17:54:47 -0700

    Completed form validation and book object initialization if validation passes on submit. This required passing the required input values to a function that checks the length of the values. If they length of the value does not pass its check, a message is then written that explains the error, and how to fix it. Those messages are then added to an object, which is then passed back to the function that is called when the submit button is clicked. Then, another check is made to see if a message exists for either of the values in the object that was returned. From there, loop over the error messages and loop the array of errMessage elements that are queried at the top, to match the class name with the value of the msg in the forEach which appends the correct message to the correct element. NOTE: I feel that this is very extra and way more complicated then it has to be. Consider another alternative

--- js/script.js
+++ js/script.js
@@ -1,28 +1,87 @@
 // ELEMENTS
-// --- Query    : query elements like the form, form elements,
-//                the submit button, etc...
+// --- Query            : query elements like the form, form elements,
+//                        the submit button, etc...
+// ~~ form controls
+const nameCntrl     = document.getElementById('nameControl')
+const authorCntrl   = document.getElementById('authorControl')
+const pagesCntrl    = document.getElementById('pagesControl')
+const formCntrls    = [nameCntrl, authorCntrl, pagesCntrl]
+// ~~ form elements
 const nameInput     = document.getElementById('name')
 const authorInput   = document.getElementById('author')
 const pagesInput    = document.getElementById('pages')
 const readInput     = document.getElementById('is_read')
+// ~~ error messages
+const nameErrMsg    = document.getElementById('nameErr')
+const authorErrMsg  = document.getElementById('authorErr')
+const pagesErrMsg   = document.getElementById('pagesErr')
+const formErrMsgs   = [nameErrMsg, authorErrMsg, pagesErrMsg]   
+// ~~ submit button
 const submitBtn     = document.getElementById('submitBtn')
+// ~~ form attributes
+const [...nameMinMax]   = [nameInput.getAttribute('minlength'), nameInput.getAttribute('maxlength')]
+const [...authorMinMax] = [authorInput.getAttribute('minlength'), authorInput.getAttribute('maxlength')]
+const [...pagesMinMax]  = [pagesInput.getAttribute('min'), pagesInput.getAttribute('max')]
+
+// EVENT LISTENERS
+// --- Submit Btn       : listens to when the submitBtn is clicked to call
+//                        the addBook function
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    // - get values from inputs
+    const nameValue     = nameInput.value
+    const authorValue   = authorInput.value
+    const pagesValue    = pagesInput.value
+    const readValue     = readInput.checked
+
+    // - pass values to function to validate form and return 
+    //   error messages where needed
+    const isValid       = validateForm(nameValue, authorValue, pagesValue)
+
+    // - check if an error message exists, then loop through object to display
+    //   message
+    if (isValid.nameErr || isValid.authorErr || isValid.pagesErr) {
+        // - get values of object to an array,
+        //      + loop over the messages
+        //      + return messages whose index value of 1 is TRUE
+        const errorMessages = Object.entries(isValid)
+        errorMessages.forEach(msg => {
+            let [el] = formErrMsgs.filter(el => el.getAttribute('id') === msg[0])
+            if (msg[1]) {
+                el.innerText = msg[1]
+            } else {
+                el.innerText = ''
+            }
+        })
+    } else {
+        // - clear error messages
+        formErrMsgs.forEach(el => {
+            el.innerText = ''
+        })
+        // - if no errors, create book object
+        const book = new Book(nameValue, authorValue, pagesValue, readValue)
+        console.log(book)
+        console.log(book.info())
+    }
+})
 
 // ARRAYS
-// --- Library  : array to store the book objects
+// --- Library          : array to store the book objects
 let myLibrary = []
 
 // OBJECTS
-// --- Book     : takes a 'name', 'author', and pages number
-//                  + has method to return a string
+// --- Book             : takes a 'name', 'author', and pages number
+//                          + has method to return a string
 class Book {
-    constructor(name, author, pages) {
+    constructor(name, author, pages, read) {
         this.name   = name
         this.author = author
         this.pages  = pages
+        this.read   = read
     }
 
     info() {
-        return this.name + ', by ' + this.author + ', ' + this.pages + ' pages'
+        return this.name + ', by ' + this.author + ', ' + this.pages + ' pages. Is read: ' + this.read
     }
 }
 
@@ -97,6 +156,42 @@ function addBookToLibrary(book) {
     myLibrary.push(book)
     displayLibrary(myLibrary)
 }
+
+// FUNCTIONS
+// --- Add Book         : runs when the submitBtn variable is clicked
+//                          + accepts the values of the for inputs
+//                              to create a book object and then push to
+//                              the library
+function addBook() {
+
+}
+// --- Validate Form    : checks form values to see if inputs are valid,
+//                        returns object of messages
+function validateForm(name, author, pages) {
+    // error messages object
+    const errMsg = {
+        nameErr     : false,
+        authorErr   : false,
+        pagesErr    : false
+    }
+
+    // values validation checks
+    const nv = name.length >= nameMinMax[0] && name.length <= nameMinMax[1]
+    const av = author.length >= authorMinMax[0] && author.length <= authorMinMax[1]
+    const pv = parseInt(pages) >= pagesMinMax[0] && parseInt(pages) <= pagesMinMax[1]
+
+    // check validation booleans (nv, av, pv), and return a text if FALSE, false if TRUE
+    errMsg.nameErr      = !nv ? 'Name of book is required and must be larger than ' + nameMinMax[0] + ' and less than ' + nameMinMax[1] : false
+    errMsg.authorErr    = !av ? 'Author is required and must be larger than ' + authorMinMax[0] + ' and less than ' + authorMinMax[1] : false
+    errMsg.pagesErr     = !pv ? 'Pages is required and must be a number between ' + pagesMinMax[0] + ' and ' + pagesMinMax[1] : false
+
+    // if err msgs exists
+    const errMsgExists = !errMsg.nameErr || !errMsg.authorErr || !errMsg.pagesErr 
+
+    // return errMsg object
+    return errMsg
+}
+
 
 let hp = new Book('Harry Potter', 'J.K. Rowling', 245)
 let lotr = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 450)
