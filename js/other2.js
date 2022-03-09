238c23ee7f39961bc8768e6b9226ebda56979d3f
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-08 18:05:04 -0700

    Can now createa storage object for my library, add to it, and look up and object by an index

--- js/other2.js
+++ js/other2.js
@@ -5,8 +5,31 @@ const library = (() => {
     // --- contains the array of library objects (book)
     const libraryStorage = (() => {
         let _library = []
+        let _uuid    = 0                 // *** fake implementation
+        // @@ GETTERS
         const getLibrary    = () => _library
         const getLibraryObj = (num) => _library[num]
-        
-    })
+        // @@ SETTERS
+        // const setLibrary
+        // !! MODIFIER FUNCTIONS
+        const addObject = (libObj) => {
+            _setUUID()
+            _library.push({_uuid,libObj})
+        }
+        // ?? HELPER FUNCTIONS
+        function _setUUID() {
+            _uuid++
+            return _uuid
+        }
+
+        return {
+            getLibrary,
+            getLibraryObj,
+            addObject
+        }
+    })()
+
+    console.log(libraryStorage.getLibrary())
+    console.log(libraryStorage.addObject('Hi'))
+    console.log(libraryStorage.getLibrary())
 })()
