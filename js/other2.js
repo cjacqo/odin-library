36d7fadcfa2c10ee1755f7d4413d784cbd3935ba
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-08 19:10:42 -0700

    Can now track values of buttons on a click event

--- js/other2.js
+++ js/other2.js
@@ -14,7 +14,10 @@ const library = (() => {
         // const setLibrary
         // !! MODIFIER FUNCTIONS
         const addObject = (libObj) => {
+            // --- create a fake uuid
             _setUUID()
+            // --- create HTML elements
+
             _library.push({_uuid,libObj})
         }
         // ?? HELPER FUNCTIONS
@@ -46,11 +49,50 @@ const library = (() => {
         function _infoToString() {
             return `${_name}, by ${_author}, ${_pages} pages. Is read: ${_read}`
         }
+        // function _createElements() {
+
+        // }
 
         return {
             getInfo
         }
     }
+
+    // HTML Elements
+    const viewCntrl = (() => {
+        const userSelection = (selection) => {
+            console.log(selection)
+        }
+
+        return {
+            userSelection
+        }
+    })()
+
+    // The Display
+    const displayCntrl = (() => {
+        // --- get HTML 
+        //     >> modal containers
+        const addFormModal      = document.querySelector('.add-form')
+        const filterFormModal   = document.querySelector('.filter-form-container')
+        //     >> data containers
+        const tableContainer    = document.getElementById('tableDisplayParent')
+        const cardsContainer    = document.getElementById('cardsDisplayParent')
+        //     >> buttons
+        const buttons           = Array.from(document.querySelectorAll('button'))
+        console.log(buttons)
+        const _init = (() => {
+            // loop over the buttons array to assign related event listeners
+            buttons.forEach(button => {
+                button.addEventListener('click', (e) => {
+                    viewCntrl.userSelection(e.currentTarget.value)
+                })
+            })
+        })()
+    })()
+
+
+    
     // const libraryObj = (() => {
     //     let _uuid    = 0                 // *** fake implementation
     // })()
@@ -64,24 +106,24 @@ const library = (() => {
     // }
 
     // ----- TEST 2 ----- //
-    // function test2() {
-    //     // Create Object
-    //     //   + log info
-    //     //   + add to storage
-    //     //   + log storage library
-    //     const obj = LibraryObj('Harry Potter', 'J.K. Rowling', 345, false, 'Fantasy')
-    //     console.log(obj.getInfo())
-    //     libraryStorage.addObject(obj)
-    //     console.log(libraryStorage.getLibrary())
-    //     // Create Second Object
-    //     //   + log info
-    //     //   + add to storage
-    //     //   + log storage library
-    //     const obj2 = LibraryObj('Lord of the Rings', 'J.R.R. Tolien', 500, true, 'Fantasy')
-    //     console.log(obj2.getInfo())
-    //     libraryStorage.addObject(obj2)
-    //     console.log(libraryStorage.getLibrary())
-    // }
-    // test2()
+    function test2() {
+        // Create Object
+        //   + log info
+        //   + add to storage
+        //   + log storage library
+        const obj = LibraryObj('Harry Potter', 'J.K. Rowling', 345, false, 'Fantasy')
+        console.log(obj.getInfo())
+        libraryStorage.addObject(obj)
+        console.log(libraryStorage.getLibrary())
+        // Create Second Object
+        //   + log info
+        //   + add to storage
+        //   + log storage library
+        const obj2 = LibraryObj('Lord of the Rings', 'J.R.R. Tolien', 500, true, 'Fantasy')
+        console.log(obj2.getInfo())
+        libraryStorage.addObject(obj2)
+        console.log(libraryStorage.getLibrary())
+    }
+    test2()
     
 })()
