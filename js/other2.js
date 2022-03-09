f45622ca0e9591fd83b470d18e4f8602b1274ded
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-08 18:31:58 -0700

    Created a factory function for a Library Object. Returns a function that gets an array of information on the object: index 0 = a readable string from values, index 1 = an object of all the values of the object

--- js/other2.js
+++ js/other2.js
@@ -5,10 +5,11 @@ const library = (() => {
     // --- contains the array of library objects (book)
     const libraryStorage = (() => {
         let _library = []
-        let _uuid    = 0                 // *** fake implementation
+        let _uuid    = 0
         // @@ GETTERS
         const getLibrary           = () => _library
         const getLibraryObjByIndex = (num) => _library[num]
+        const getSize              = () => _library.length
         // @@ SETTERS
         // const setLibrary
         // !! MODIFIER FUNCTIONS
@@ -25,12 +26,62 @@ const library = (() => {
         return {
             getLibrary,
             getLibraryObjByIndex,
+            getSize,
             addObject
         }
     })()
 
-    console.log(libraryStorage.getLibrary())
-    libraryStorage.addObject('Hi')
-    console.log(libraryStorage.getLibrary())
-    console.log(libraryStorage.getLibraryObjByIndex(0))
+    // Library Obj
+    // --- defaulted to book: will set values, uuid, and HTML elements
+    const LibraryObj = (name, author, pages, read, category) => {
+        const _name     = name
+        const _author   = author
+        const _pages    = pages
+        const _read     = read
+        const _category = category
+        // @@ GETTERS
+        const getInfo   = () => [_infoToString(), {_name, _author, _pages, _read, _category}]
+
+        // ?? HELPER FUNCTIONS
+        function _infoToString() {
+            return `${_name}, by ${_author}, ${_pages} pages. Is read: ${_read}`
+        }
+
+        return {
+            getInfo
+        }
+    }
+    // const libraryObj = (() => {
+    //     let _uuid    = 0                 // *** fake implementation
+    // })()
+    // >> TESTS
+    // ----- TEST 1 ----- //
+    // function test1() {
+    //     console.log(libraryStorage.getLibrary())
+    //     libraryStorage.addObject('Hi')
+    //     console.log(libraryStorage.getLibrary())
+    //     console.log(libraryStorage.getLibraryObjByIndex(0))
+    // }
+
+    // ----- TEST 2 ----- //
+    // function test2() {
+    //     // Create Object
+    //     //   + log info
+    //     //   + add to storage
+    //     //   + log storage library
+    //     const obj = LibraryObj('Harry Potter', 'J.K. Rowling', 345, false, 'Fantasy')
+    //     console.log(obj.getInfo())
+    //     libraryStorage.addObject(obj)
+    //     console.log(libraryStorage.getLibrary())
+    //     // Create Second Object
+    //     //   + log info
+    //     //   + add to storage
+    //     //   + log storage library
+    //     const obj2 = LibraryObj('Lord of the Rings', 'J.R.R. Tolien', 500, true, 'Fantasy')
+    //     console.log(obj2.getInfo())
+    //     libraryStorage.addObject(obj2)
+    //     console.log(libraryStorage.getLibrary())
+    // }
+    // test2()
+    
 })()
